073b586fb68ca904527b7e6ea1ebc6e0dc41757a
royWati <royywati@gmail.com>
2019-01-09 09:44:40 +0300

    contains class and object definitions

---
+++ ClassesAndObjects/src/Bus.java
@@ -0,0 +1,35 @@
+/* this object is called a bus
+    it is missing a the package name because the java
+    class belongs to a default package called src
+
+    however all the classes belonging to carmodels
+    would contain the package header for the location.
+
+ */
+
+
+public class Bus {
+
+    /* Bus is our object class
+        Since it is an object it will require
+        a field
+        methods
+
+        we normally store variables within a field
+        in orderr to provide an easier access of the
+        variables to the methods/procedures
+
+        ALL OBJECTS REQUIRE A MAIN METHOD IN ORDER TO
+        EXECUTE.
+
+     */
+
+    public static void main(String[] args){
+        System.out.println("This is our first program");
+        /* System.out.println() is a method that is default to object
+            that is used to print/output data on the console.
+
+         */
+    }
+
+}
---
+++ ClassesAndObjects/src/carmodels/Toyota.java
@@ -0,0 +1,4 @@
+package carmodels;
+
+public class Toyota {
+}
